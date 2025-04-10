c57483647e8113542977320d4181e6c49a1b94e2
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-04-10 21:30:25 +0000

    rename(simulation->aegis_athena_contracts)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use tonic::{Request, Response, Status};
-use simulation::simulation_service_server::{SimulationService, SimulationServiceServer};
-use simulation::{SimulationBatchRequest, SimulationBatchResult, SimulationScenario, Portfolio};
-use rand::SeedableRng;
-use rand::Rng;
-use rand_distr::Normal;
-use tonic::transport::Server;
-use simulation::simulation_service_server::SimulationServiceServer;
+use my_crate::Sampler;
 use my_crate::SimulationServiceImpl; // Adjust to your crate's module structure.
-use my_crate::Sampler;               // Import your sampler type.
+use rand::Rng;
+use rand::SeedableRng;
+use rand_distr::Normal;
+use aegis_athena_contracts::simulation_service_server::{SimulationService, SimulationServiceServer};
+use aegis_athena_contracts::{Portfolio, SimulationBatchRequest, SimulationBatchResult, SimulationScenario};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status}; // Import your sampler type.
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,13 +15,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Define the address where the Athena simulation service will listen.
     let addr = "0.0.0.0:50051".parse()?;
-    
+
     // Create an instance of your Sampler.
     let sampler = Sampler::default();
-    
+
     // Instantiate your simulation service with the sampler.
     let simulation_service = SimulationServiceImpl { sampler };
-    
+
     println!("Athena Simulation Service listening on {}", addr);
 
     // Build and serve the gRPC server.
@@ -33,4 +32,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-
